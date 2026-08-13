anomalyco/opencode

fix(desktop): make file:// chat links clickable
DOMPurify was stripping file: hrefs, and the desktop click handler always
routed through openExternal. Keep file: links on desktop, open them via
the existing openPath/revealPath IPC (http(s) still go through openLink,
hardened to http(s)-only).

Fixes #37891

packages/desktop/src/renderer/index.tsx
@@ -27,6 +27,7 @@ import { DesktopFirstLaunchOnboarding } from "./onboarding"
 import { resetZoom, setPinchZoomEnabled, webviewZoom, zoomIn, zoomOut } from "./webview-zoom"
 import { windowFullscreen } from "./window-fullscreen"
 import { availableStartupServer, readyWslConnections } from "./wsl/connections"
+import { pathFromFileHref } from "./file-link"
 import "./styles.css"
 import { Splash } from "@opencode-ai/ui/logo"
 import { useTheme } from "@opencode-ai/ui/theme/context"
@@ -360,10 +361,25 @@ function DesktopRoot(props: { windowState: DesktopWindowState }) {
 
   function handleClick(e: MouseEvent) {
     const link = (e.target as HTMLElement).closest("a.external-link") as HTMLAnchorElement | null
-    if (link?.href) {
-      e.preventDefault()
-      platform.openLink(link.href)
+    if (!link?.href) return
+    e.preventDefault()
+
+    // Prefer the raw attribute so file: links aren't rewritten against oc://renderer.
+    const href = link.getAttribute("href") || link.href
+    const filePath = pathFromFileHref(href)
+    if (filePath) {
+      const reveal = e.metaKey || e.ctrlKey || e.altKey || e.button === 1
+      if (reveal && platform.revealPath) {
+        void platform.revealPath(filePath)
+        return
+      }
+      if (platform.openPath) {
+        void platform.openPath(filePath)
+        return
+      }
     }
+
+    platform.openLink(link.href)
   }
 
   function Inner() {

kianwoon/opencode-app

packages/desktop/src/renderer/index.tsx
@@ -28,6 +28,7 @@ import { DesktopFirstLaunchOnboarding } from "./onboarding"
 import { resetZoom, setPinchZoomEnabled, webviewZoom, zoomIn, zoomOut } from "./webview-zoom"
 import { windowFullscreen } from "./window-fullscreen"
 import { availableStartupServer, readyWslConnections } from "./wsl/connections"
+import { pathFromFileHref } from "./file-link"
 import "./styles.css"
 import { Splash } from "@opencode-ai/ui/logo"
 import { useTheme } from "@opencode-ai/ui/theme/context"
@@ -355,6 +356,28 @@ function DesktopRoot(props: { windowState: DesktopWindowState }) {
   )
   const onboarding = Promise.withResolvers<void>()
 
+  function handleClick(e: MouseEvent) {
+    const link = (e.target as HTMLElement).closest("a.external-link") as HTMLAnchorElement | null
+    if (!link?.href) return
+    e.preventDefault()
+
+    // Prefer the raw attribute so file: links aren't rewritten against oc://renderer.
+    const href = link.getAttribute("href") || link.href
+    const filePath = pathFromFileHref(href)
+    if (filePath) {
+      const reveal = e.metaKey || e.ctrlKey || e.altKey || e.button === 1
+      if (reveal && platform.revealPath) {
+        void platform.revealPath(filePath)
+        return
+      }
+      if (platform.openPath) {
+        void platform.openPath(filePath)
+        return
+      }
+    }
+
+    platform.openExternal(link.href)
+  }
   function Inner() {
     const cmd = useCommand()
     menuTrigger = (id) => cmd.trigger(id)